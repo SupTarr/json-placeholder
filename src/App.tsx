a635aef261bc40350d1b389b831c12ce39d70113
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2023-12-21 13:18:04 +0700

    feat: add tabs to show different data

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+enum Tabs {
+  Users,
+  Posts,
+  Comments,
+}
+
+const App = () => {
+  const [tab, setTab] = useState(Tabs.Users);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <div role="tablist" className="tabs-boxed tabs">
+        <a
+          role="tab"
+          className={`tab ${tab === Tabs.Users ? "tab-active" : ""}`}
+          onClick={_ => setTab(Tabs.Users)}
+        >
+          {Tabs[Tabs.Users]}
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a
+          role="tab"
+          className={`tab ${tab === Tabs.Posts ? "tab-active" : ""}`}
+          onClick={_ => setTab(Tabs.Posts)}
+        >
+          {Tabs[Tabs.Posts]}
+        </a>
+        <a
+          role="tab"
+          className={`tab ${tab === Tabs.Comments ? "tab-active" : ""}`}
+          onClick={_ => setTab(Tabs.Comments)}
+        >
+          {Tabs[Tabs.Comments]}
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
-}
+};
 
 export default App;
